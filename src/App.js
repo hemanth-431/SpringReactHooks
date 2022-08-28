69a20d01b18f05faa6761b26829047d39d8c56c2
hemanth <bandlavhemanth@gmail.com>
2022-08-28 10:03:44 +0530

    Date Hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from "react";
+import {useEffect } from "react";
+import DatePicker from 'react-date-picker';
 function App() {
+
+const [count, setCount] = useState(0);
+
+  useEffect(() => {
+
+     let timer=setTimeout(()=>{setCount(count+1);
+   }, [count]);
+
+     return () => clearTimeout(timer)
+     }, []);
+
+const [value, onChange] = useState(new Date());
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <DatePicker onChange={onChange} value={value} />
+         <h1>I have rendered {count} times!</h1>
+
+         <button onClick={() => setCount((c) => c + 1)}>+</button>
+
+
       </header>
+
+
     </div>
   );
 }
